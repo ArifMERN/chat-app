a573b91ac0156f12dbcdb8c51424f95cf0699a60
ArifMERN <arifmern@gmail.com>
2022-11-01 16:15:01 +0530

    configured the dropdown app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const App = () => {
+  const display = () => {
+    document.getElementById("drop-down").classList.add("show");
+    document.getElementById("drop-down").classList.remove("hide");
+  };
+  const hidden = () => {
+    document.getElementById("drop-down").classList.add("hide");
+    document.getElementById("drop-down").classList.remove("show");
+  };
+  const data = ["one", "two", "three"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <select name="" id="drop-down" className="show">
+          {data.map((val, i) => (
+            <option value={i}>{val}</option>
+          ))}
+        </select>
+        <h3>Should you use Dropdown?</h3>
+        <button id="yes" onMouseOver={display}>
+          Yes
+        </button>
+        <button id="no" onMouseOver={hidden}>
+          Probably not
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
